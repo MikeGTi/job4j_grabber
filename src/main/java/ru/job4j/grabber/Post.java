44b8ed2fe3fa6e68ca5a50448a824afbc527b6e5
MikeGTi <mikegti@ya.com>
2024-03-06 22:17:20 +0300

    2.2. Модель данных - Post. [#285211 #484832]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -74,18 +74,14 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Post post)) {
-            return false;
-        }
-        return Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description);
+        if (this == o) return true;
+        if (!(o instanceof Post post)) return false;
+        return getId() == post.getId() && Objects.equals(getLink(), post.getLink());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link, description);
+        return Objects.hash(getId(), getLink());
     }
 
     @Override
