9bf8b541161b1d1757734f542948d8d0ae107787
MikeGTi <mikegti@ya.com>
2024-03-06 12:45:37 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/datamodel/Post.java
@@ -0,0 +1,61 @@
+package ru.job4j.grabber.datamodel;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Post post)) {
+            return false;
+        }
+        return Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, link, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created +
+                '}';
+    }
+}
