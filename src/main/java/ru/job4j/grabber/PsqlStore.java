685f284f823079aaeb90ebd8d51579a56175b7c1
MikeGTi <mikegti@ya.com>
2024-03-13 21:39:47 +0300

    7. Web. [#289478] - correction

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -81,8 +81,8 @@ public class PsqlStore implements Store {
         return new Post(
                         resultSet.getInt("id"),
                         resultSet.getString("name"),
-                        resultSet.getString("text"),
                         resultSet.getString("link"),
+                        resultSet.getString("text"),
                         (resultSet.getTimestamp("created")).toLocalDateTime()
                 );
     }
